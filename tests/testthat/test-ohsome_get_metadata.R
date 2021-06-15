fb7e4a7f69973ab196b4b930e7ca4e3967a53c78
Oliver Fritz <oliver.fritz@heigit.org>
2021-06-15 17:34:08 +0100

    add tests for ohsome_get_metadata with mock api

--- tests/testthat/test-ohsome_get_metadata.R
+++ tests/testthat/test-ohsome_get_metadata.R
@@ -1,3 +1,16 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+with_mock_api({
+	test_that(
+		"ohsome_get_metadata assigns metadata to ohsome_metadata", {
+			ohsome_get_metadata()
+			expect_equal(ohsome_metadata$apiVersion, "1.4.1")
+			expect_type(ohsome_metadata, "list")
+		})
+
+	test_that(
+		"ohsome_get_metadata returns metadata", {
+			meta <- ohsome_get_metadata()
+			expect_equal(meta$apiVersion, "1.4.1")
+			expect_type(meta, "list")
+		})
 })
+
