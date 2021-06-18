9b82fbd2901ac2c3c925b27a86a99276384f81eb
Oliver Fritz <oliver.fritz@heigit.org>
2021-06-18 09:47:32 +0100

    harmonize ohsome metadata documentation with https://docs.ohsome.org/ohsome-api/v1/response-parameters.html

--- R/ohsome_get_metadata.R
+++ R/ohsome_get_metadata.R
@@ -13,18 +13,20 @@
 #'     attributes \code{date} and \code{status_code} (of the GET request) and
 #'     the following list elements:
 #'     \describe{
-#'         \item{attribution}{\code{url} and \code{text} of OSM data attribution
-#'             (character)}
-#'         \item{apiVersion}{Current version number of ohsome API
+#'         \item{attribution}{\code{url} and \code{text} of OSM data copyrights
+#'             and attribution (character)}
+#'         \item{apiVersion}{Version of the ohsome API
 #'             (numeric_version)}
-#'         \item{timeout}{Maximum timeout for ohsome API requests in seconds
-#'             (numeric)}
+#'         \item{timeout}{Limit of the processing time in seconds (numeric)}
 #'         \item{extractRegion}{
-#'             \code{spatialExtent}: {spatial extent of available data
-#'                 (sfc_POLYGON)}\cr
-#'             \code{temporalExtent}: {temporal extent of available data
-#'                 (vector of POSIXct)}\cr
-#'             \code{replicationSequenceNumber}
+#'             \code{spatialExtent}: {Spatial boundary of the OSM data in the
+#'                 underlying OSHDB (sfc_POLYGON)}\cr
+#'             \code{temporalExtent}: {Timeframe of the OSM data in the
+#'                 underlying OSHDB data (vector of POSIXct)}\cr
+#'             \code{replicationSequenceNumber}: {Precise state of the OSM data
+#'                 contained in the underlying OSHDB, expressed as the id of the
+#'                 last applied (hourly) diff file from
+#'                 \link{planet.openstreetmap.org} (numeric)}
 #'     }}
 #' @seealso \url{https://docs.ohsome.org/ohsome-api/v1/endpoints.html#metadata}
 #' @export
