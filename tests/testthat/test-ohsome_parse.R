f135b9ba6bbefbe3da85c52db85f70c9afabc726
Oliver Fritz <oliver.fritz@heigit.org>
2021-06-24 14:11:50 +0100

    extend test-ohsome_parse

--- tests/testthat/test-ohsome_parse.R
+++ tests/testthat/test-ohsome_parse.R
@@ -12,13 +12,8 @@ with_mock_api({
 	r <- ohsome_post(q, parse = FALSE)
 
 	test_that(
-		'returns character if return_class = "character"', {
-			expect_type(ohsome_parse(r, return_class = "character"), "character")
-		})
-
-	test_that(
-		'returns list if return_class = "list"', {
-			expect_type(ohsome_parse(r, return_class = "list"), "list")
+		'returns data.frame by default when content CSV', {
+			expect_s3_class(ohsome_parse(r), "data.frame")
 		})
 
 	test_that(
@@ -28,7 +23,40 @@ with_mock_api({
 		})
 
 	test_that(
-		'returns data.frame if return_class = "default" and content CSV', {
-			expect_s3_class(ohsome_parse(r, return_class = "default"), "data.frame")
+		'returns list if return_class = "list"', {
+			expect_type(ohsome_parse(r, return_class = "list"), "list")
+		})
+
+	test_that(
+		'returns character if return_class = "character"', {
+			expect_type(ohsome_parse(r, return_class = "character"), "character")
+		})
+})
+
+with_mock_api({
+
+	.mockPaths("./mock_api/200")
+
+	# mock response: tests/testthat/mock_api/200/api.ohsome.org/v1/elements/centroid-ba0611-POST.csv
+	q <- ohsome_query(
+		c("elements", "centroid"),
+		filter = "shop=convenience",
+		bcircles = "13.45,52.5,1000"
+	)
+	r <- ohsome_post(q, parse = FALSE)
+
+	test_that(
+		'returns sf by default when content GeoJSON', {
+			expect_s3_class(ohsome_parse(r, return_class = "sf"), "sf")
+		})
+
+	test_that(
+		'returns data.frame when return_class = "data.frame" and content GeoOJSON', {
+			expect_s3_class(ohsome_parse(r, return_class = "data.frame"), "data.frame")
+		})
+
+	test_that(
+		'returns list if return_class = "list" and content GeoJSON', {
+			expect_type(ohsome_parse(r, return_class = "list"), "list")
 		})
 })
