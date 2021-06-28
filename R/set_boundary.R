623b79f0025a82183126a77603c9404cb1abd097
Oliver Fritz <oliver.fritz@heigit.org>
2021-06-28 10:25:52 +0100

    add basic set_boundary

---
+++ R/set_boundary.R
@@ -0,0 +1,32 @@
+#' Set boundary
+#'
+#' Modify the spatial filter of an existing \code{ohsome_query} object
+#'
+#' \code{set_boundary()} adds or modifies aspatial filter of an
+#' \code{ohsome_query} object. The spatial filter of a query to the ohsome API
+#' can be defined as dne or more polygons, bounding boxes or bounding circles.
+#'
+#' @param query An \code{ohsome_query} object
+#' @param boundary
+#' @param boundary_type
+#' @return An \code{ohsome_query} object
+#' @seealso \url{https://docs.ohsome.org/ohsome-api/v1/}
+#' @export
+#' @examples
+set_boundary <- function(
+	query,
+	boundary,
+	boundary_type = c("bpolys", "bboxes", "bcircles")
+
+) {
+	choices <- c("bpolys", "bboxes", "bcircles")
+	boundary_type <- match.arg(boundary_type)
+
+	endpoint <- gsub("^.*?/", "", httr::parse_url(query$url)$path)
+	body <- query$body
+
+	body[[boundary_type]] <- boundary
+	body[choices[choices != boundary_type]] <- NULL
+
+	return(do.call(ohsome_query, c(endpoint, body)))
+}
