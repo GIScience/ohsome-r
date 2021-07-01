acd316e33e14e00bbc41c741de3ee6cc0f182338
Oliver Fritz <oliver.fritz@heigit.org>
2021-07-01 12:54:50 +0100

    improve function docs

--- R/ohsome_aggregate_elements.R
+++ R/ohsome_aggregate_elements.R
@@ -1,17 +1,29 @@
-#' Aggregate elements
+#' Aggregate OSM elements
 #'
 #' Create an \code{ohsome_query} object for OSM element aggregation
 #'
-#' @param boundary
-#' @param boundary_type
-#' @param aggregation
-#' @param ...
+#' \code{ohsome_aggregate_elements} creates an \code{ohsome_query} object for
+#' OSM element aggregation. \code{ohsome_elements_count},
+#' \code{ohsome_elements_length}, \code{ohsome_elements_perimeter} and
+#' \code{ohsome_elements_area} are wrapper functions for specific aggregation
+#' endpoints. Boundary objects are passed via \code{\link{set_boundary}} into
+#' \code{\link{ohsome_boundary}}.
+#'
+#' @param boundary Boundary object that can be interpreted by
+#'     \code{\link{ohsome_boundary}}
+#' @param aggregation Aggregation operation
+#' @param ... Parameters of the request to the ohsome API endpoint
 #'
 #' @family Aggregate elements
 #' @return An \code{ohsome_query} object
 #' @seealso \url{https://docs.ohsome.org/ohsome-api/stable/endpoints.html#elements-aggregation}
 #' @export
 #' @examples
+#'
+#' ohsome_aggregate_elements(mapview::franconia, "count", filter = "craft=brewery")
+#' ohsome_elements_count(mapview::franconia, filter = "craft=brewery")
+#' ohsome_elements_length(mapview::franconia, filter = "highway=*")
+#'
 ohsome_aggregate_elements <- function(
 	boundary,
 	aggregation = c("count", "length", "perimeter", "area"),
--- R/ohsome_query.R
+++ R/ohsome_query.R
@@ -1,18 +1,19 @@
-#' Create an ohsome_query object
+#' Create an \code{ohsome_query} object
 #'
-#' Creates an ohsome_query object specifying the ohsome API endpoint and the
-#' request parameters.
+#' Creates an \code{ohsome_query} object specifying the ohsome API endpoint and
+#' the request parameters.
 #'
 #' @param endpoint The path to the ohsome API endpoint. Either a single string
 #' (e.g. \code{"elements/count"}) or a character vector in the right order
 #' (e.g. \code{c("elements", "count")})
 #' @param ... Parameters of the request to the ohsome API endpoint
-#' @param validate logical If true, issues warning for invalid endpoint or
+#' @param validate logical If TRUE, issues warning for invalid endpoint or
 #'      invalid/missing query parameters
 #' @return An \code{ohsome_query} object
 #' @seealso \url{https://docs.ohsome.org/ohsome-api/v1/}
 #' @export
 #' @examples
+#' ohsome_query("elements/geometry", bbox = "8.5992,49.3567,8.7499,49.4371", filter = "building=*")
 ohsome_query <- function(endpoint, ...,	validate = FALSE) {
 
 	body <- lapply(list(...), paste, collapse=",")
