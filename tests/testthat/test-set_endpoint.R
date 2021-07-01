657892968b568bfb26b4368ea1ccd1a1d4bd4ff7
Oliver Fritz <oliver.fritz@heigit.org>
2021-07-01 11:04:31 +0100

    add tests for set_endpoint

--- tests/testthat/test-set_endpoint.R
+++ tests/testthat/test-set_endpoint.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("replaces endpoint in ohsome_query by default (append = FALSE)", {
+
+	q1 <- ohsome_query("elements/count")
+	q2 <- set_endpoint(q1, "elements/length")
+	expect_equal(httr::parse_url(q2$url)$path, "v1/elements/length")
+})
+
+test_that("appends endpoint if append = TRUE", {
+
+	q1 <- ohsome_query("elements/count")
+	q2 <- set_endpoint(q1, c("groupBy", "boundary"), append = TRUE)
+	expect_equal(httr::parse_url(q2$url)$path, "v1/elements/count/groupBy/boundary")
+})
+
+test_that("resets response format correctly according to reset_format argument",{
+	q1 <- ohsome_query("elements/count")
+	q2 <- set_endpoint(q1, c("groupBy", "boundary"), append = TRUE, reset_format = FALSE)
+	q3 <- set_endpoint(q1, c("groupBy", "boundary"), append = TRUE)
+	q4 <- set_endpoint(q1, c("elements/geometry"))
+	expect_equal(q2$body$format, "csv")
+	expect_equal(q3$body$format, "geojson")
+	expect_null(q4$body$format)
 })
