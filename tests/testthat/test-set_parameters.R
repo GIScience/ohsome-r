39945614f34cb1e949fa49e36e7231f7774b3e66
Oliver Fritz <oliver.fritz@heigit.org>
2021-07-01 11:53:01 +0100

    add test for set_parameters

--- tests/testthat/test-set_parameters.R
+++ tests/testthat/test-set_parameters.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("sets and modifies parameters of ohsome_query", {
+
+	q <- ohsome_query("elements/count") %>%
+		set_parameters(foo = "bar", foo2 = "bar") %>%
+		set_parameters(foo = "baz")
+
+	expect_equal(q$body$foo, "baz")
+	expect_equal(q$body$foo2, "bar")
 })
