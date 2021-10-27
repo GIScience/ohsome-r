47f7948205076cbf9f46454cf184e5d2a1ebebeb
Oliver Fritz <oliver.fritz@heigit.org>
2021-10-27 17:29:05 +0200

    add wrappers for contributions extraction endpoints

---
+++ R/ohsome_extract_contributions.R
@@ -0,0 +1,73 @@
+#' Extract OSM contributions
+#'
+#' Create an \code{ohsome_query} object for OSM contribution extraction
+#'
+#' \code{ohsome_extract_contributionss} creates an \code{ohsome_query} object 
+#' for OSM contribution extraction. \code{ohsome_contributions_bbox},
+#' \code{ohsome_contributions_centroid} and \code{ohsome_contributions_geometry} 
+#' are wrapper functions for specific contributions extraction endpoints. 
+#' Boundary objects are passed via \code{\link{set_boundary}} into 
+#' \code{\link{ohsome_boundary}}.
+#'
+#' @param boundary Boundary object that can be interpreted by
+#'     \code{\link{ohsome_boundary}}
+#' @param geometryType Type of geometry to be extracted. Can be centroids 
+#'     (\code{centroid}, bounding boxes (\code{bbox}) or \code{geometry}. 
+#'     Default: \code{centroid}. Caveat: Node elements are omitted from results 
+#'     in queries for bounding boxes. 
+#' @param time must consist of two ISO-8601 conform timestrings (as 
+#'     comma-separated character or as character vector of length two) defining 
+#'     a time interval (defaults to the temporal extent of the underlying OSHDB)
+#' @param properties Can be "tags" to extract all tags and/or "metadata" to 
+#'     provide metadata (\code{changesetId}, \code{lastEdit}, \code{osmType} and
+#'     \code{version}) and/or "contributionTypes" to provide contribution types
+#'     with the contributions. Multiple values can be provided as 
+#'     comma-separated character or as character vector. 
+#'     Default: NULL (provides \code{contributionChangesetId}, \code{osmId} and 
+#'     \code{timestamp} with the contribution geometries)
+#' @param clipGeometry logical Specifies whether the returned geometries of the 
+#'     contributions should be clipped to the query’s spatial boundary
+#' @param ... Parameters of the request to the ohsome API endpoint
+#'
+#' @family Extract contributions
+#' @return An \code{ohsome_query} object
+#' @seealso \url{https://docs.ohsome.org/ohsome-api/stable/endpoints.html#contributions-extraction}
+#' @export
+#' @examples
+#' 
+#' # query to extract contributions to man-made features around "null island" with metadata:
+#' ohsome_contributions_geometry("0,0,10", filter = "man_made=*", properties = "metadata")
+
+ohsome_extract_contributions <- function(
+	boundary = NULL,
+	geometryType = c("centroid", "bbox", "geometry"),
+	time = lubridate::format_ISO8601(.ohsome_metadata$extractRegion$temporalExtent),
+	properties = NULL,
+	clipGeometry = TRUE,
+	...
+) {
+	geometryType <- match.arg(geometryType)
+	q <- ohsome_query(c("contributions", geometryType), boundary, ...)
+	q <- set_properties(q, properties)
+	q <- set_parameters(q, clipGeometry = as.character(clipGeometry))
+	q <- set_time(q, paste(time, collapse = ","))
+	return(q)
+}
+
+#' @export
+#' @rdname ohsome_extract_contributions
+ohsome_contributions_bbox <- function(boundary = NULL, ...) {
+	ohsome_extract_contributions(boundary, geometryType = "bbox", ...)
+}
+
+#' @export
+#' @rdname ohsome_extract_contributions
+ohsome_contributions_centroid <- function(boundary = NULL, ...) {
+	ohsome_extract_contributions(boundary, geometryType = "centroid", ...)
+}
+
+#' @export
+#' @rdname ohsome_extract_contributions
+ohsome_contributions_geometry <- function(boundary = NULL, ...) {
+	ohsome_extract_contributions(boundary, geometryType = "geometry", ...)
+}
---
+++ tests/testthat/test-ohsome_extract_contributions.R
@@ -0,0 +1,20 @@
+test_that("creates ohsome_query object with the correct endpoint", {
+	q <- ohsome_contributions_geometry()
+	expect_equal(httr::parse_url(q$url)$path, "v1/contributions/geometry")
+})
+
+test_that("returns object of class ohsome_query", {
+	expect_s3_class(ohsome_extract_contributions(), "ohsome_query")
+})
+
+test_that("correctly sets clipGeometry parameter", {
+	q <- ohsome_contributions_geometry(clipGeometry = FALSE)
+	expect_equal(q$body$clipGeometry, "FALSE")
+})
+
+test_that("uses full temporal extent if time parameter is missing", {
+	expect_equal(
+		ohsome_extract_contributions()$body$time,
+		paste(lubridate::format_ISO8601(.ohsome_metadata$extractRegion$temporalExtent), collapse = ",")
+	)
+})
