6922c152729dcf38e699631f35e4610b1aea59bc
Oliver Fritz <oliver.fritz@heigit.org>
2022-03-09 18:54:13 +0100

    improve doc

--- R/ohsome_aggregate_elements.R
+++ R/ohsome_aggregate_elements.R
@@ -1,55 +1,60 @@
 #' Aggregate OSM elements
 #'
-#' Create an \code{ohsome_query} object for OSM element aggregation
+#' Creates an `ohsome_query` object for OSM element aggregation
 #'
-#' \code{ohsome_aggregate_elements} creates an \code{ohsome_query} object for
-#' OSM element aggregation. \code{ohsome_elements_count},
-#' \code{ohsome_elements_length}, \code{ohsome_elements_perimeter} and
-#' \code{ohsome_elements_area} are wrapper functions for specific aggregation
-#' endpoints. Boundary objects are passed via \code{\link{set_boundary}} into
-#' \code{\link{ohsome_boundary}}.
+#' `ohsome_aggregate_elements()` creates an `ohsome_query` object for
+#' OSM element aggregation. `ohsome_elements_count()`,
+#' `ohsome_elements_length()`, `ohsome_elements_perimeter()` and
+#' `ohsome_elements_area()` are wrapper functions for specific aggregation
+#' endpoints. Boundary objects are passed via [set_boundary()] into
+#' [ohsome_boundary()].
 #'
-#' @param boundary Boundary object that can be interpreted by
-#'     \code{\link{ohsome_boundary}}
+#' @inheritParams ohsome_query
 #' @param aggregation character; aggregation operation
 #' @param return_value character; the value to be returned by the ohsome API:
-#'  \describe{
-#'         \item{absolute}{Returns the absolute number, length, perimeter or area
-#'         of elements (default).}
-#'         \item{density}{Returns the number, length, perimeter or area
-#'         of elements per square kilometer.}
-#'         \item{ratio}{Returns an absolute \code{value} for elements satisfying 
-#'         the \code{filter} argument, an absolute \code{value2} for elements 
-#'         satisfying the \code{filter2} argument, and the \code{ratio} of 
-#'         \code{value2} to \code{value}}
-#' }
-#' @param grouping character; group type(s) for grouped aggregations. The 
-#' following group types are available:
-#' \describe{
-#'         \item{boundary}{Groups the result by the given boundaries that are defined through any of the boundary query parameters}
-#'         \item{key}{Groups the result by the given keys that are defined through the groupByKeys query parameter.}
-#'         \item{tag}{Groups the result by the given tags that are defined through the groupByKey and groupByValues query parameters.}
-#'         \item{type}{Groups the result by the given OSM, or simple feature types that are defined through the types parameter.}
-#'         \item{c("boundary", "tag")}{Groups the result by the given boundary and the tags.}
-#' }
-#' Not all of these group types are accepted by all of the aggregation 
-#' endpoints. Check 
-#' [Grouping](https://docs.ohsome.org/ohsome-api/v1/group-by.html) 
-#' for available group types.
+#'   * `"absolute"` returns the absolute number, length, perimeter or area of 
+#'   elements. This is the default.
+#'   * `"density"` returns the number, length, perimeter or area of elements per 
+#'   square kilometer.
+#'   * `"ratio"` returns an absolute `value` for elements satisfying the 
+#'   `filter` argument, an absolute `value2` for elements satisfying the 
+#'   `filter2` argument, and the `ratio` of `value2` to `value`.
 #' @param time character; time parameter of the query (see 
-#'     [Supported time formats](https://docs.ohsome.org/ohsome-api/v1/time.html); 
-#'     defaults to most recent available timestamp in the underlying OSHDB)
-#' @param ... Parameters of the request to the ohsome API endpoint
-#'
-#' @family Aggregate elements
-#' @return An \code{ohsome_query} object
-#' @seealso \url{https://docs.ohsome.org/ohsome-api/stable/endpoints.html#elements-aggregation}
+#'   [Supported time formats](https://docs.ohsome.org/ohsome-api/v1/time.html); 
+#'   defaults to most recent available timestamp in the underlying OSHDB)
+#' @inherit ohsome_query return
+#' @seealso [ohsome API Endpoints - Elements Aggregation](https://docs.ohsome.org/ohsome-api/stable/endpoints.html#elements-aggregation)
 #' @export
+#' @family Aggregate elements
 #' @examples
-#'
+#' # Count of breweries in Franconia
 #' ohsome_aggregate_elements(mapview::franconia, "count", filter = "craft=brewery")
 #' ohsome_elements_count(mapview::franconia, filter = "craft=brewery")
-#' ohsome_elements_length(mapview::franconia, filter = "highway=*")
+#' 
+#' # Monthly counts of breweries in Franconia from 2010 to 2020
+#' ohsome_elements_count(
+#'     mapview::franconia, 
+#'     filter = "craft=brewery", 
+#'     time = "2010/2020/P1M"
+#' )
+#' 
+#' # Count of breweries per district of Franconia
+#' ohsome_elements_count(
+#'     mapview::franconia, 
+#'     filter = "craft=brewery", 
+#'     grouping = "boundary"
+#' )
+#' 
+#' # Proportion of breweries in Franconia that are Microbreweries
+#' ohsome_elements_count(
+#'     mapview::franconia, 
+#'     filter = "craft=brewery", 
+#'     filter2 = "craft=brewery and microbrewery=yes", 
+#'     return_value = "ratio"
+#' ) 
+#' 
+#' # Total length of highway elements in Franconia
+#' ohsome_elements_length(mapview::franconia, filter = "highway=* and geometry:line")
 #'
 ohsome_aggregate_elements <- function(
 	boundary = NULL,
